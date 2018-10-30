ea062efcc865ea5ef66f85c9565fa5c1f84c7377
dlin2028 <dlin2028@gmail.com>
2018-10-29 20:51:41 -0700

    initial

---
+++ HillClimber/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace HillClimber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string target = Console.ReadLine();
+            char[] current = new char[target.Length];
+            int lastError = int.MaxValue;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                current[i] += (char)rnd.Next(32, 126);
+            }
+
+            int error = 0;
+            int mutation;
+            while (target.ToCharArray() != current)
+            {
+                do
+                {
+                    mutation = rnd.Next(-1, 2);
+                } while (mutation == 0);
+
+                int index = rnd.Next(target.Length);
+                
+                current[index] = (char)(current[index] + mutation);
+                if (current[index] < 32)
+                {
+                    current[index] = (char)126;
+                }
+                else if (current[index] > 126)
+                {
+                    current[index] = (char)32;
+                }
+
+                error = 0;
+                for (int j = 0; j < current.Length; j++)
+                {
+                    error += Math.Abs(current[j] - target[j]);
+                }
+
+                if (error > lastError)
+                {
+                    current[index] = (char)(current[index] - mutation);
+                    if (current[index] < 32)
+                    {
+                        current[index] = (char)126;
+                    }
+                    else if (current[index] > 126)
+                    {
+                        current[index] = (char)32;
+                    }
+                    error = lastError;
+                }
+                else
+                {
+                    lastError = error;
+                }
+                Console.WriteLine($"{new string(current)} error: {error}");
+            }
+        }
+    }
+}
+
+
